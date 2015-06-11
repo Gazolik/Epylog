85cedc220a2dfe8ab51363fdfd4322c8d0225c6d
Nicolas Nativel <nicolas@cassoulet.k>
2015-06-11 15:22:57 +0200

    Modification schema

--- Parser.py
+++ Parser.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -10,12 +11,18 @@ class Player(Base):
     id = Column(Integer, primary_key=True)
     pseudo = Column(String)
 
+class PlayerGame(Base):
+    __tablename__ = 'playerGame'
+    game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
+    player = Column(String, ForeignKey('player.pseudo'), primary_key=True)
+    kill = Column(String)
 
 class Game(Base):
     __tablename__ = 'game'
     id = Column(Integer, primary_key=True)
-    player = Column(String, ForeignKey('player.pseudo'), primary_key=True)
-    kill = Column(String)
+    map_name = Column(String)
+    termination = Column(String)
+
 
 def parse_line(line):
     splited_line = line.split(' ')
@@ -30,7 +37,7 @@ def parse_line(line):
         pass
 
 
-engine = create_engine('sqlite:///tmp/test.db')
+engine = create_engine('sqlite:///')
 session = sessionmaker()
 session.configure(bind=engine)
 Base.metadata.create_all(engine)
