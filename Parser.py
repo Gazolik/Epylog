df1e0da6bd2e949afedbe1a7da792139e64d54ae
Nicolas Nativel <nicolas@cassoulet.k>
2015-06-11 14:25:47 +0200

    Ajout du parser

---
+++ Parser.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Player(Base):
+    __tablename__ = 'player'
+    id = Column(Integer, primary_key=True)
+    pseudo = Column(String)
+
+
+class Game(Base):
+    __tablename__ = 'game'
+    id = Column(Integer, primary_key=True)
+    player = Column(String, ForeignKey('player.pseudo'), primary_key=True)
+    kill = Column(String)
+
+def parse_line(line):
+    splited_line = line.split(' ')
+    if splited_line[0] == "ClientConnect:":
+        client_info = f.readline().split('\\')
+        player = Player(pseudo=client_info[1])
+    elif splited_line[0] == "Kill:":
+        pass
+    elif splited_line[0] == "Item:":
+        pass
+    elif splited_line[0] == "Exit:":
+        pass
+
+
+engine = create_engine('sqlite:///tmp/test.db')
+session = sessionmaker()
+session.configure(bind=engine)
+Base.metadata.create_all(engine)
+
+
+with open('logTest.log', 'r') as f:
+    for line in f:
+        parse_line(line)
