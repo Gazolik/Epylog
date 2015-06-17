22dcc32e13522bf70b4dab552c63cd7d1ebf88da
= <=>
2015-06-17 14:00:51 +0200

    ajout du timestamp

--- epylog/logparser.py
+++ epylog/logparser.py
@@ -1,8 +1,7 @@
-from .model import Game, Player, PlayerGame, connection, Kill, Weapon
-
+from .model import Game, Player, connection, Kill, Weapon
+import datetime
 
 current_game = Game(map_name=None, termination=None)
-player_game_list = {}  # (pseudo, player_game)
 player_id_matching = {}  # (id ingame du joueur, pseudo)
 player_id_matching['1022'] = 'world'
 weapon_list = connection.query(Weapon).all()
@@ -10,16 +9,30 @@ weapon_id_matching = {}
 for i in weapon_list:
     weapon_id_matching[i.id] = i
 kills_list = []
+try:
+    lastDate = open('lastDate', 'r')
+    lastExit = float(lastDate.readline())
+    print('Last exit : '+str(lastExit))
+except IOError:
+    lastExit = 0
+    print('file doesn t exist')
+else:
+    lastDate.close()
 
-with open('logTest.log', 'r') as f:
+with open('timeStamp.log', 'r') as f:
     for line in f:
         splited_line = line.split(' ')
-        if splited_line[0] == 'Kill:':
-            id_killer = splited_line[1]
-            id_killed = splited_line[2]
-            id_weapon = splited_line[3][0:-1]
+        time = float(splited_line[0])
+        if(time <= lastExit):
+            continue
+        timestamp = datetime.datetime.fromtimestamp(time)
+        if splited_line[1] == 'Kill:':
+            id_killer = splited_line[2]
+            id_killed = splited_line[3]
+            id_weapon = splited_line[4][0:-1]
             kill = Kill()
             kill.game = current_game
+            kill.time = timestamp
             if(id_killer == '1022'):
                 id_killer = id_killed
             kill.player_killer = connection.query(Player).filter(
@@ -28,40 +41,36 @@ with open('logTest.log', 'r') as f:
                 Player.pseudo == player_id_matching[id_killed]).first()
             kill.weapon = weapon_id_matching[int(id_weapon)]
             kills_list.append(kill)
-            if id_killer != id_killed and id_killer != '1022':
-                player_game_list[player_id_matching[id_killer]].kill += 1
-                player_game_list[player_id_matching[id_killer]].score += 1
-            else:
-                player_game_list[player_id_matching[id_killed]].score -= 1
-            player_game_list[player_id_matching[id_killed]].death += 1
-        elif splited_line[0] == 'Item:':
-            id_looter = splited_line[1]
-            name_item = splited_line[2][0:-1]
+        elif splited_line[1] == 'Item:':
+            id_looter = splited_line[2]
+            name_item = splited_line[3][0:-1]
             name_player = player_id_matching[id_looter]
-        elif splited_line[0] in ('Rcon', 'InitGame:'):
+        elif splited_line[1] in ('Rcon', 'InitGame:'):
             connection.rollback()
-            player_game_list.clear()
             kills_list.clear()
-            if splited_line[0] == 'Rcon':
-                name = splited_line[4][0:-1]
+            if splited_line[1] == 'Rcon':
+                name = splited_line[5][0:-1]
             else:
                 split_list = line.split('\\')
                 name = split_list[split_list.index('mapname')+1]
-            current_game = Game(map_name=name, termination=None)
+            current_game = Game(map_name=name,
+                                termination=None, starting_time=timestamp)
             connection.add(current_game)
-        elif splited_line[0] == 'Exit:':
-            end = splited_line[1]
+        elif splited_line[1] == 'Exit:':
+            end = splited_line[2]
+            print('Exit')
             current_game.termination = end
-            for pseudo, player_game in player_game_list.items():
-                connection.add(player_game)
+            current_game.ending_time = timestamp
             for kill in kills_list:
                 connection.add(kill)
             connection.commit()
             kills_list.clear()
-            player_game_list.clear()
             player_id_matching['1022'] = 'world'
-        elif splited_line[0] == 'ClientUserinfoChanged:':
-            ingame_id = splited_line[1]
+            with open('lastDate', 'w') as lastDate:
+                lastDate.write(splited_line[0])
+            lastExit = float(splited_line[0])
+        elif splited_line[1] == 'ClientUserinfoChanged:':
+            ingame_id = splited_line[2]
             name_player = line.split('\\')[1]
             player_id_matching[ingame_id] = name_player
             player = connection.query(Player).filter(
@@ -69,13 +78,5 @@ with open('logTest.log', 'r') as f:
             if player is None:
                 player = Player(pseudo=name_player)
                 connection.add(player)
-            player_game = connection.query(PlayerGame).filter(
-                PlayerGame.game == current_game,
-                PlayerGame.player == player).first()
-            if player_game is None:
-                player_game = PlayerGame(kill=0, death=0, score=0)
-                player_game.player = player
-                player_game.game = current_game
-                player_game_list[player.pseudo] = player_game
-        elif splited_line[0] == 'ClientDisconnect:':
-            del player_id_matching[splited_line[1][0:-1]]
+        elif splited_line[1] == 'ClientDisconnect:':
+            del player_id_matching[splited_line[2][0:-1]]
--- epylog/model.py
+++ epylog/model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, PickleType
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import (Column, String, Integer,
+                        ForeignKey, create_engine, DateTime)
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -10,17 +11,6 @@ class Player(Base):
     __tablename__ = 'player'
     id = Column(Integer, primary_key=True)
     pseudo = Column(String)
-    player_game = relationship('PlayerGame', backref='player')
-    
-    
-class PlayerGame(Base):
-    __tablename__ = 'playergame'
-    game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
-    player_id = Column(Integer, ForeignKey('player.id'), primary_key=True)
-    kill = Column(Integer)
-    death = Column(Integer)
-    score = Column(Integer)
-
 
 
 class Kill(Base):
@@ -28,10 +18,11 @@ class Kill(Base):
     id = Column(Integer, primary_key=True)
     game_id = Column(Integer, ForeignKey('game.id'))
     player_killer_id = Column(Integer, ForeignKey('player.id'))
-    player_killer = relationship('Player', foreign_keys = [player_killer_id])
+    player_killer = relationship('Player', foreign_keys=[player_killer_id])
     player_killed_id = Column(Integer, ForeignKey('player.id'))
-    player_killed = relationship('Player', foreign_keys = [player_killed_id])
+    player_killed = relationship('Player', foreign_keys=[player_killed_id])
     weapon_id = Column(Integer, ForeignKey('weapon.id'))
+    time = Column(DateTime)
 
 
 class Game(Base):
@@ -39,12 +30,14 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     map_name = Column(String)
     termination = Column(String)
-    player_game = relationship('PlayerGame', backref='game')
-    kills = relationship('Kill',backref ='game')
+    kills = relationship('Kill', backref='game')
+    starting_time = Column(DateTime)
+    ending_time = Column(DateTime)
+
 
 class Weapon(Base):
     __tablename__ = 'weapon'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     weapon_name = Column(String)
     kills = relationship('Kill', backref='weapon')
 
