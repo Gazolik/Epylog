511e8e351a7108f730b9453eb80d09ba6f8f84f9
Nicolas NATIVEL <n.nativel@kozea.fr>
2015-06-17 14:22:57 +0200

    Ajout properties player, ajout templates

--- epylog/model.py
+++ epylog/model.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, PickleType
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import Column, String, Integer, ForeignKey, create_engine, 
+from sqlalchemy.orm import relationship, backref, sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import func
+from operator import itemgetter
 
 Base = declarative_base()
 
@@ -12,7 +14,63 @@ class Player(Base):
     pseudo = Column(String)
     player_game = relationship('PlayerGame', backref='player')
     
+    @hybrid_property
+    def kill_sum(self):
+        return (
+            db_session
+            .query(func.count(Kill.player_killed_id))
+            .filter_by(player_killer_id=self.id)
+            .filter(Kill.player_killer_id != Kill.player_killed_id)
+            .group_by(Kill.player_killer_id)
+            .scalar()
+            )
+
+    @hybrid_property
+    def killed_sum(self):
+        return (
+            db_session
+            .query(func.count(Kill.player_killer_id))
+            .filter_by(player_killed_id=self.id)
+            .filter(Kill.player_killer_id != Kill.player_killed_id)
+            .group_by(Kill.player_killed_id)
+            .scalar()
+            )
+
+    @hybrid_property
+    def death_sum(self):
+        return (
+            db_session
+            .query(func.count(Kill.player_killer_id))
+            .filter_by(player_killed_id=self.id)
+            .group_by(Kill.player_killed_id)
+            .scalar()
+            )
+
+    @hybrid_property
+    def weapon_statitics(self):
+        return (
+            db_session
+            .query(Kill.weapon_id, func.count(Kill.weapon_id))
+            .filter_by(player_killed_id=self.id)
+            .group_by(Kill.weapon_id)
+            )
+
+    @hybrid_property
+    def favorite_weapon(self):
+        w_id = max(self.weapon_statistic, key=itemgetter(1))[0]
+        return Weapon.query.filter(id=w_id).one()
+
+    @property
+    def ratio_kill_killed(self):
+        return round((self.kill_sum or 0) / (self.killed_sum or 1), 2)
+ 
+    @property
+    def ratio_kill_death(self):
+        return round((self.kill_sum or 0) / (self.death_sum or 1), 2)
     
+
+        
+
 class PlayerGame(Base):
     __tablename__ = 'playergame'
     game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
@@ -22,15 +80,14 @@ class PlayerGame(Base):
     score = Column(Integer)
 
 
-
 class Kill(Base):
     __tablename__ = 'kill'
     id = Column(Integer, primary_key=True)
     game_id = Column(Integer, ForeignKey('game.id'))
     player_killer_id = Column(Integer, ForeignKey('player.id'))
-    player_killer = relationship('Player', foreign_keys = [player_killer_id])
+    player_killer = relationship('Player', foreign_keys=[player_killer_id])
     player_killed_id = Column(Integer, ForeignKey('player.id'))
-    player_killed = relationship('Player', foreign_keys = [player_killed_id])
+    player_killed = relationship('Player', foreign_keys=[player_killed_id])
     weapon_id = Column(Integer, ForeignKey('weapon.id'))
 
 
@@ -40,16 +97,24 @@ class Game(Base):
     map_name = Column(String)
     termination = Column(String)
     player_game = relationship('PlayerGame', backref='game')
-    kills = relationship('Kill',backref ='game')
+    kills = relationship('Kill', backref='game')
+
 
 class Weapon(Base):
     __tablename__ = 'weapon'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     weapon_name = Column(String)
     kills = relationship('Kill', backref='weapon')
+
 
 engine = create_engine('sqlite:////tmp/database')
 session = sessionmaker()
 session.configure(bind=engine)
 Base.metadata.create_all(engine)
 connection = session()
+
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,
+                bind=engine))
+Base.query = db_session.query_property()
+
+
