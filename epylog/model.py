4cc6e13af6ae5e184d7cb111992c87306b257583
= <=>
2015-06-18 14:21:55 +0200

    clean

--- epylog/model.py
+++ epylog/model.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 class Player(Base):
     __tablename__ = 'player'
     id = Column(Integer, primary_key=True)
-    pseudo = Column(String)
+    pseudo = Column(String, unique=True)
 
     @hybrid_property
     def kill_sum(self):
