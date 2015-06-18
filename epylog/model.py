cdbc67fa61d1b12c369826d63bfb3e91b1aa4149
Nicolas NATIVEL <n.nativel@kozea.fr>
2015-06-18 11:55:08 +0200

    Modif des templates, ajouts de routes pour graph, map , historic et weapon

--- epylog/model.py
+++ epylog/model.py
@@ -1,9 +1,9 @@
 from sqlalchemy import (Column, String, Integer,
                         ForeignKey, create_engine, DateTime)
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy import func
+from sqlalchemy import func, or_
 from operator import itemgetter
 
 Base = declarative_base()
@@ -13,7 +13,6 @@ class Player(Base):
     __tablename__ = 'player'
     id = Column(Integer, primary_key=True)
     pseudo = Column(String)
-    player_game = relationship('PlayerGame', backref='player')
     
     @hybrid_property
     def kill_sum(self):
@@ -48,18 +47,29 @@ class Player(Base):
             )
 
     @hybrid_property
-    def weapon_statitics(self):
+    def weapon_statistics(self):
         return (
             db_session
             .query(Kill.weapon_id, func.count(Kill.weapon_id))
-            .filter_by(player_killed_id=self.id)
+            .filter_by(player_killer_id=self.id)
+            .filter(Kill.player_killed_id != Kill.player_killer_id)
             .group_by(Kill.weapon_id)
             )
 
     @hybrid_property
     def favorite_weapon(self):
-        w_id = max(self.weapon_statistic, key=itemgetter(1))[0]
-        return Weapon.query.filter(id=w_id).one()
+        w_id = max(self.weapon_statistics, key=itemgetter(1))[0]
+        return Weapon.query.get(w_id)
+    
+    @hybrid_property
+    def total_game_played(self):
+        return (
+            Kill.query
+            .filter(or_(Kill.player_killer_id == self.id,
+                Kill.player_killed_id == self.id))
+            .count()
+            )
+
 
     @property
     def ratio_kill_killed(self):
@@ -69,18 +79,6 @@ class Player(Base):
     def ratio_kill_death(self):
         return round((self.kill_sum or 0) / (self.death_sum or 1), 2)
     
-
-        
-
-class PlayerGame(Base):
-    __tablename__ = 'playergame'
-    game_id = Column(Integer, ForeignKey('game.id'), primary_key=True)
-    player_id = Column(Integer, ForeignKey('player.id'), primary_key=True)
-    kill = Column(Integer)
-    death = Column(Integer)
-    score = Column(Integer)
-
-
 
 class Kill(Base):
     __tablename__ = 'kill'
@@ -99,7 +97,6 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     map_name = Column(String)
     termination = Column(String)
-
     kills = relationship('Kill', backref='game')
     starting_time = Column(DateTime)
     ending_time = Column(DateTime)
--- epylog/routes.py
+++ epylog/routes.py
@@ -1,7 +1,7 @@
-from .model import engine, Base, Player, PlayerGame, Game, Kill, Weapon, db_session
-from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from sqlalchemy import func 
+from .model import Player, Game, Weapon, db_session
+from flask import Flask, render_template, make_response
+from sqlalchemy import desc 
+import pygal 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -11,7 +11,12 @@ def home_page():
     top_players = Player.query.all()
     sorted_players = sorted(
         top_players, key=lambda p: p.ratio_kill_killed, reverse=True)
-    return render_template('home_page.html', top_players=sorted_players[:3])
+    game_historic = Game.query.order_by(desc(Game.ending_time)).limit(5)
+    return render_template(
+        'home_page.html', 
+        top_players=sorted_players[:3],
+        game_historic=game_historic
+        )
 
 
 @app.route('/playerslist')
@@ -19,7 +24,7 @@ def show_players_list():
     top_players = Player.query.all()
     sorted_players = sorted(
         top_players, key=lambda p: p.ratio_kill_killed, reverse=True)
-    return render_template('home_page.html', top_players=sorted_players)
+    return render_template('player_list.html', top_players=sorted_players)
  
 
 @app.route('/playerdetails/<pseudo>')
@@ -30,7 +35,39 @@ def show_player_details(pseudo):
     return render_template('player_details.html', player=player)
 
 
-@app.route('/weapongraph/<pseudo>')
+@app.route('/weapongraph/<pseudo>.svg')
 def generate_weapon_graph(pseudo):
-    kill = db_session.query(Kill.weapon_id).group_by(Kill.weapon_id).having(player_killer_id=pseudo)
-    print(kill)
+    radar_chart = pygal.Radar()
+    radar_chart.title = '{} Weapon use'.format(pseudo)
+    labels = []
+    values = []
+    for row in Player.query.filter_by(pseudo=pseudo).first().weapon_statistics:
+        labels.append(Weapon.query.get(row[0]).weapon_name)
+        values.append(row[1])
+    radar_chart.x_labels = labels
+    radar_chart.add('Weapon use', values)
+    svg = radar_chart.render()
+    response = make_response(svg)
+    response.content_type = 'image/svg+xml'
+    return response
+
+
+@app.route('/map_list/')
+def show_map_list():
+    map_list = db_session.query(Game.map_name).group_by(Game.map_name)
+    # interesting stats : -most deadly weapon, avg game duration, 
+
+@app.route('/map_details/<map_name>')
+def show_map_details():
+    pass
+                            
+@app.route('/gamehistoric')
+def show_game_historic():
+    game_historic = Game.query.order_by(desc(Game.ending_time))
+    return render_template('game_historic.html', game_historic=game_historic)
+
+@app.route('/weapon')
+def show_weapon_statistics():
+    pass
+
+
