1c05c8716bff6931f71c72c104c1bb80528f90d4
= <=>
2015-06-19 16:57:43 +0200

    code clean

--- epylog/model.py
+++ epylog/model.py
@@ -50,89 +50,30 @@ class Player(Base):
     def weapon_statistics(self):
         return (
             db_session
-            .query(Kill.weapon_id,
-            func.count(Kill.weapon_id).label('kill_count'))
+            .query(Kill.weapon_id, func.count(Kill.weapon_id))
             .filter_by(player_killer_id=self.id)
             .filter(Kill.player_killed_id != Kill.player_killer_id)
             .group_by(Kill.weapon_id)
-            .order_by('kill_count desc')
-            .all()
             )
 
+    @hybrid_property
     def favorite_weapon(self):
-        if self.weapon_statistics:
-            return Weapon.query.get(self.weapon_statistics[0][0])
-        else:
-            return None
+        w_id = max(self.weapon_statistics, key=itemgetter(1))[0]
+        return Weapon.query.get(w_id)
 
     @hybrid_property
     def total_game_played(self):
         return (
             Kill.query
             .filter(or_(Kill.player_killer_id == self.id,
-                Kill.player_killed_id == self.id))
-            .group(Kill.game_id)
+                        Kill.player_killed_id == self.id))
             .count()
             )
 
-    def most_killed_player(self):
-        player = (
-                db_session
-                .query(Kill.player_killed_id,
-                    func.count(Kill.player_killed_id).label('kill_count'))
-                .filter_by(player_killer_id=self.id)
-                .filter(Kill.player_killer_id != Kill.player_killed_id)
-                .group_by(Kill.player_killed_id)
-                .order_by('kill_count desc').first()
-                )
-        if player: 
-            return Player.query.get(player[0])
-        else:
-            return None
-
-    def most_killed_by_player(self):
-        player = (
-                db_session
-                .query(Kill.player_killer_id, 
-                    func.count(Kill.player_killer_id).label('kill_count'))
-                .filter_by(player_killed_id=self.id)
-                .filter(Kill.player_killer_id != Kill.player_killed_id)
-                .group_by(Kill.player_killer_id)
-                .order_by('kill_count desc').first()
-                )
-        if player: 
-            return Player.query.get(player[0])
-        else:
-            return None
-
-    def kill_list(self, number=None):
-        return [p.pseudo for p in (
-            db_session
-            .query(Player.pseudo)
-            .select_from(Kill)
-            .join(Kill.player_killed)
-            .filter(Kill.player_killer_id == self.id)
-            .order_by(Kill.time.desc())
-            .limit(number)
-            .all())
-            ]
-
-    def killed_list(self, number=None):
-        return [p.pseudo for p in (
-            db_session
-            .query(Player.pseudo)
-            .select_from(Kill)
-            .join(Kill.player_killer)
-            .filter(Kill.player_killed_id == self.id)
-            .order_by(Kill.time.desc())
-            .limit(number)
-            .all())
-            ]
-        
     @property
     def ratio_kill_killed(self):
         return round((self.kill_sum or 0) / (self.killed_sum or 1), 2)
- 
+
     @property
     def ratio_kill_death(self):
         return round((self.kill_sum or 0) / (self.death_sum or 1), 2)
@@ -155,6 +96,7 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     map_name = Column(String)
     termination = Column(String)
+
     kills = relationship('Kill', backref='game')
     starting_time = Column(DateTime)
     ending_time = Column(DateTime)
@@ -167,10 +109,8 @@ class Weapon(Base):
     kills = relationship('Kill', backref='weapon')
 
 
-
-
 engine = create_engine('postgresql://epylog@localhost/epylog')
-session = sessionmaker(bind = engine)
+session = sessionmaker(bind=engine)
 connection = session()
 Base.metadata.create_all(engine)
 connection = session()
@@ -178,5 +118,3 @@ connection = session()
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,
                                          bind=engine))
 Base.query = db_session.query_property()
-
-
